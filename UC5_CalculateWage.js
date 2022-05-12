62389045bc1489ae019f414680db00c1db5bb126
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-05-13 00:45:59 +0530

    Added: Calculate Wage till the Condition of Total Working hrs or Max days

---
+++ UC5_CalculateWage.js
@@ -0,0 +1,67 @@
+//UC 1
+const IS_ABSENT=0
+let empCheck=Math.floor(Math.random()*10)%2;
+if(empCheck==IS_ABSENT){
+    console.log("Employee is Absent");
+    
+}else{
+    console.log("Employee is present");
+}
+//UC 2
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HOURS=4;
+const FULL_TIME_HOURS=8;
+const WAGE_PER_HOUR=20;
+let empHrs=0;
+empCheck=Math.floor(Math.random()*10) % 3;
+switch(empCheck){
+    case IS_PART_TIME:
+        empHrs=PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+            empHrs=FULL_TIME_HOURS;
+            break;
+    default:
+        empHrs=0;
+}
+let empWage=empHrs*WAGE_PER_HOUR;
+console.log("Emp Wage: "+empWage);
+//UC 3
+function getWorkingHours(empCheck){
+    switch(empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+            case IS_FULL_TIME:
+                return FULL_TIME_HOURS;
+                default:
+                    return 0;
+    }
+}
+ empHrs=0;
+ empCheck=Math.floor(Math.random()*10) % 3;
+empHrs=getWorkingHours(empCheck);
+ empWage=empHrs*WAGE_PER_HOUR;
+console.log("Emp Wage: "+empWage);
+//UC 4
+const Num_OF_Working_Days=2;
+empHrs=0;
+for(let day=0; day<Num_OF_Working_Days;day++){
+    let empCheck=Math.floor(Math.random()*10) % 3;
+    empHrs +=getWorkingHours(empCheck);
+    }
+     empWage=empHrs*WAGE_PER_HOUR;
+     console.log("Total HRS: " +empHrs+ "Emp Wage: " +empWage);
+
+     //UC 5
+     const MAX_HRS_IN_MONTH=100;
+     const NUM_OF_WORKING_DAYS=10;
+     let totalEmpHrs=0;
+     let totalWorkingDays=0;
+     while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<Num_OF_Working_Days){
+         totalWorkingDays++;
+         let empCheck=Math.floor(Math.random()*10) % 3;
+         totalEmpHrs += getWorkingHours(empCheck);
+     }
+      empWage=totalEmpHrs*WAGE_PER_HOUR;
+     console.log("UC5: Total Days:" + totalWorkingDays +  "Total Hrs:"  + totalEmpHrs +  "Emp Wage: "  + empWage);
